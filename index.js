2b23610e2f0443a289dbee8edcfff7f3cab9897b
larrynburris <larry.n.burris@gmail.com>
2017-10-19 13:00:17 -0400

    Update server to use TriviaQuestionBank

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 const http = require("http");
 const express = require("express");
-var FloodData = require('./flood-data');
-var FloodEvent = require('./flood-event');
-var FloodStatsGenerator = require('./flood-stats');
+var TriviaQuestionBank = require('./trivia-question-bank');
 
-var dataService = new FloodData();
-var data = dataService.loadFromFile('./data/floods.xls');
-var floodEvents = data.map(function(flood) {
-    return new FloodEvent(flood);
- });
-//console.log(floodEvents[0].printFull());
-//console.log(floodEvents[1].printFull());
-console.log(floodEvents.length);
-
-var floodStatsGenerator = new FloodStatsGenerator(floodEvents);
-var yearCounts = floodStatsGenerator.getYearlyFloodCounts();
-console.log(yearCounts);
+let triviaQuestionBank = new TriviaQuestionBank();
 
 let app = express();
 app.server = http.createServer(app);
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3001;
 
 app.use('/', express.static('public'));
+
+app.get('/trivia/:questionNumber', function(req, res) {
+    let questionNumber = parseInt(req.params.questionNumber);
+    let question =  triviaQuestionBank.getQuestion(questionNumber);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(question.toJSON()));
+});
+
+app.post('/trivia/tally/:questionNumber/:choice', function(req, res) {
+    let questionNumber = parseInt(req.params.questionNumber);
+    let choice = isNaN(req.params.choice) ? req.params.choice : parseInt(req.params.choice);
+    let question = triviaQuestionBank.updateQuestion(questionNumber, choice);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(question.toJSON()));
+});
 
 app.server.listen(port, () => {
     console.log(`Started on port ${app.server.address().port}`);
